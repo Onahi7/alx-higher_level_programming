484a2cc05308418329e8cf448efcddda193c36a1
Onahi7 <dicksonhardy7@gmail.com>
2023-10-09 15:10:26 -0700

    hardy

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,8 @@
+def element_at(my_list, idx):
+    """Retrieve an element from a list like in C."""
+    if idx < 0:
+        return None
+    elif idx >= len(my_list):
+        return None
+    else:
+        return my_list[idx]
